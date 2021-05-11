d107804af621ba81f1b178ec265e08c91994e5b6
Jeferson Leon <jefersonleonblue@gmail.com>
2021-05-11 17:30:51 -0300

    Projeto Criado dia 11/05/2021 - Exemplo de um loop utilizando o while e com repetição indeterminado -  usuário digita valores gastos- ao final o programa mostra o total gasto da mesada - utilizando acumuladores Revisão final

--- src/projetoacumulador/ProjetoAcumulador.java
+++ src/projetoacumulador/ProjetoAcumulador.java
@@ -10,7 +10,7 @@ public class ProjetoAcumulador {
 
     public static void main(String[] args) {
        // vamos criar um controle de mesada
-        Scanner ler = new Scanner(System.in);
+          Scanner ler = new Scanner(System.in);
        double mesada, valor, acumulador=0;
        int cont=1;
        mesada = 300;
@@ -20,16 +20,18 @@ public class ProjetoAcumulador {
            if(cont!=0){
            System.out.println("Informe o valor do gasto:");
            valor = ler.nextDouble();
-           
+           if (valor< 0 ||  valor>mesada){
+               System.out.println("Impossivel gastar esse valor! ou vc está trapaceando seu usurio xiruzão");
+           }else{           
            acumulador = acumulador + valor;
            mesada = mesada - valor;
            System.out.println("Valor total gasto R$"+acumulador);
            System.out.println("Valor restante da mesada R$"+mesada);
+           }
            }//fim do if
        }//fim do loop
            System.out.println("Bye!!!!");
            System.out.println("Valor final gasto R$"+acumulador);
            System.out.println("Valor restante da mesada R$"+mesada);
        }
-          
 }
